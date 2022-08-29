a6bf031efaf748983e649fb4d4e651de4f2a4c27
Priya Ganeshan <112310220+Priyamahaa@users.noreply.github.com>
2022-08-29 21:58:10 +0530

    Add files via upload

---
+++ javascript/ab.js
@@ -0,0 +1,27 @@
+var a=1;
+var b=10;
+var c=a+b;
+document.write(c+"<br>");
+
+var a=1;
+var b=10;
+var c=55;
+var d=a+b+c;
+document.write(d);
+
+
+
+var a=92;
+var b=15;
+var c=a-b;
+document.write(c);
+
+var a=32;
+var b=15;
+var c=a*b;
+document.write(c);
+
+var a=45;
+var b=9;
+var c=a/b;
+document.write(c);
